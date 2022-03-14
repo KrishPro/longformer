0d7bf7b584877e9e4bf69aa6e12a1a9a1e29849b
Krish Baisoya <62995566+KrishPro@users.noreply.github.com>
2022-03-14 17:25:43 +0530

    removed all dependencies to avoid version issues

--- setup.py
+++ setup.py
@@ -3,16 +3,12 @@
 from setuptools import setup
 
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
-
 setup(
     name='longformer',
     version='0.1',
     packages=['longformer', 'longformer.lib', 'tvm', 'tvm._ffi', 'tvm._ffi._ctypes', 'tvm.contrib'],
     package_data={'tvm': ['*.so'], 'longformer': ['lib/*.so']},
     entry_points='',
-    install_requires=required,
+    install_requires=[],
 )
 
