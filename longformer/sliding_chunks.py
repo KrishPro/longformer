12ac93c03c80bab5b0f5d8eca88525f6d4bca6e7
Krish Baisoya <62995566+KrishPro@users.noreply.github.com>
2022-03-14 20:00:02 +0530

    Upgraded the way to check if XLA is con figured

--- longformer/sliding_chunks.py
+++ longformer/sliding_chunks.py
@@ -1,8 +1,13 @@
 import torch
 import torch.nn.functional as F
 from longformer.diagonaled_mm_tvm import mask_invalid_locations
-from .xla import XLA_AVAILABLE
 
+try:
+    import torch_xla.core.xla_model as xm
+except ImportError:
+    XLA_AVAILABLE = False
+else:
+    XLA_AVAILABLE = True
 
 def _skew(x, direction, padding_value):
     '''Convert diagonals into columns (or columns into diagonals depending on `direction`'''
--- longformer/xla.py
+++
@@ -1,6 +0,0 @@
-try:
-    import torch_xla
-    torch.tensor(True, device="xla:1")
-    XLA_AVAILABLE = True
-except:
-    XLA_AVAILABLE = False
